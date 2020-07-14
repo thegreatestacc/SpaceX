ad5b48f7fc4ac19979332d953f66739e1da7397c
thegreatestacc <67763738+thegreatestacc@users.noreply.github.com>
2020-07-14 18:01:23 +0300

    last change

---
+++ src/main/java/net/sovliv/domain/Response.java
@@ -0,0 +1,11 @@
+/*package net.sovliv.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Response")
+public class Response {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}*/
